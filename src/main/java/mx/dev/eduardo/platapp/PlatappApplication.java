51c5bf889f99b139d9377129b86ab45dff37278d
edarriagagdl <edarriaga@gmail.com>
2024-09-12 00:48:06 -0600

    primeraclase

---
+++ src/main/java/mx/dev/eduardo/platapp/PlatappApplication.java
@@ -0,0 +1,13 @@
+package mx.dev.eduardo.platapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PlatappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PlatappApplication.class, args);
+	}
+
+}
---
+++ src/test/java/mx/dev/eduardo/platapp/PlatappApplicationTests.java
@@ -0,0 +1,13 @@
+package mx.dev.eduardo.platapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PlatappApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
